b1e67f2705050bf91c0fdc67b685cabdeab47b11
Raven <raven.xu98@gmail.com>
2021-03-15 21:48:00 +0800

    fix: optimize styles

--- utils/packagelost_test.py
+++ utils/packagelost_test.py
@@ -1,28 +1,3 @@
-# -*- coding: utf-8 -*-
-#
-#     ||          ____  _ __
-#  +------+      / __ )(_) /_______________ _____  ___
-#  | 0xBC |     / __  / / __/ ___/ ___/ __ `/_  / / _ \
-#  +------+    / /_/ / / /_/ /__/ /  / /_/ / / /_/  __/
-#   ||  ||    /_____/_/\__/\___/_/   \__,_/ /___/\___/
-#
-#  Copyright (C) 2017 Bitcraze AB
-#
-#  Crazyflie Nano Quadcopter Client
-#
-#  This program is free software; you can redistribute it and/or
-#  modify it under the terms of the GNU General Public License
-#  as published by the Free Software Foundation; either version 2
-#  of the License, or (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-#  MA  02110-1301, USA.
 """
 This script shows the basic use of the MotionCommander class.
 
@@ -66,13 +41,16 @@ def packagelost_pos_callback(timestamp, data, logconf):
 
 
 if __name__ == '__main__':
-    uri = 'radio://0/' + sys.argv[1] + '/2M'
-    logname = sys.argv[2]
+    uri_id = sys.argv[1]
+    log_name = sys.argv[2]
+    uri = 'radio://0/' + uri_id + '/2M'
+    log_path = "./log/" + log_name + ".log"
+    fd = open(log_path, "w+")
 
-    fd = open("./log/" + logname + ".log", "w+")
     # Initialize the low-level drivers (don't list the debug drivers)
     cflib.crtp.init_drivers(enable_debug_driver=False)
     with SyncCrazyflie(uri, cf=Crazyflie(rw_cache='./cache')) as scf:
+        # Config the log
         logconf = LogConfig(name='Stabilizer', period_in_ms=10)
         logconf.add_variable('Packagelost.notolsr', 'int16_t')
         logconf.add_variable('Packagelost.recvcountgt', 'int16_t')
@@ -80,9 +58,11 @@ if __name__ == '__main__':
         scf.cf.log.add_config(logconf)
         logconf.data_received_cb.add_callback(packagelost_pos_callback)
         logconf.start()
-        # We take off when the commander is created
+
+        # Take off
         with MotionCommander(scf, default_height=0.5) as mc:
             time.sleep(10)
             mc.stop()
-            # We land when the MotionCommander goes out of scope
+
+        # Land when the MotionCommander goes out of scope
         logconf.stop()
